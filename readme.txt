146e2ed42ba078f15d86f75a42c7fb4b5f1f754e
sreeparnadas <sreeparnadas0675@gmail.com>
2024-08-22 00:38:34 +0530

    custom sidenav and header added

--- readme.txt
+++ readme.txt
@@ -6,8 +6,19 @@ ng generate component footer
 Q) How to Create Standalone Components in Angular 17
 ng generate component <component-name> --standalone
 
-https://youtu.be/c8YGsPx0zVk?si=VgW8PYRMSFfyJ0hT
+Ref: https://youtu.be/c8YGsPx0zVk?si=VgW8PYRMSFfyJ0hT
+https://youtu.be/_7kbgMu8XbI?si=7oSM4q0sNgzsJBrv
 
 
 Q) Where will you store project images?
 Put images in public folder. https://stackoverflow.com/a/78637673/26549614
+
+
+
+demo site
+https://www.phpscriptsonline.com/product/exam-management/
+
+mat-icons
+https://www.angularjswiki.com/angular/angular-material-icons-list-mat-icon-list/
+
+Q) Custom-side-nav guide Ref: [https://www.youtube.com/watch?v=cz3pQQ5owd0&t=44s, https://www.youtube.com/watch?v=Sq5tdKKBz7Q]
--- src/app/custom-sidenav/custom-sidenav.component.ts
+++ src/app/custom-sidenav/custom-sidenav.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route: string
+}
 
 @Component({
   selector: 'app-custom-sidenav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatListModule, MatIconModule],
   templateUrl: './custom-sidenav.component.html',
   styleUrl: './custom-sidenav.component.css'
 })
 export class CustomSidenavComponent {
-
+  menuItem = signal<MenuItem[]>([
+    {
+      icon: 'home',
+      label: 'Home',
+      route: 'home'
+    },
+    {
+      icon: 'people',
+      label: 'Student Details',
+      route: 'student-details'
+    },
+    {
+      icon: 'book',
+      label: 'Course Management',
+      route: 'course-management'
+    },
+    {
+      icon: 'subject',
+      label: 'Subject List',
+      route: 'subject-list'
+    },
+    {
+      icon: 'laptop',
+      label: 'Exam Management',
+      route: 'exam-management'
+    },
+    {
+      icon: 'question_answer',
+      label: 'Manage Question Bank',
+      route: 'manage-question-bank'
+    },
+    {
+      icon: 'code',
+      label: 'Online Test',
+      route: 'online-test'
+    },
+    {
+      icon: 'grade',
+      label: 'Result',
+      route: 'result'
+    },
+    {
+      icon: 'settings',
+      label: 'Settings',
+      route: 'settings'
+    },
+  ])
 }
